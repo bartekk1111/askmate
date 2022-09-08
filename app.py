b76ee9902d72d932b7c82fb83b525525cf10c511
bartekk1111 <bartosz_keskrawiec@wp.pl>
2022-09-08 22:10:31 +0200

    imports modify in app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-from email.policy import default
-from turtle import title
 from urllib import request
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-import data_handler
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
