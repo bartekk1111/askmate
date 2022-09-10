07ad91b2f4cc11c5cf0863a81fd22d62c16b22fd
bartekk1111 <bartosz_keskrawiec@wp.pl>
2022-09-10 10:28:48 +0200

    New table in database, Update is working

--- app.py
+++ app.py
@@ -7,17 +7,31 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
+class Questions(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
+    content = db.Column(db.String(1000), nullable=False)
+    author = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer)
     date_created = db.Column(db.DateTime, default=datetime.utcnow())
     view_number = db.Column(db.Integer, default=1)
     vote_number = db.Column(db.Integer, default=1)
-    title = db.Column(db.String(200), nullable=False)
-    image_id = db.Column(db.Integer, default=0)
+    image_path = db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
+
+# class Answers(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     answer = db.Column(db.String(200), nullable=False)
+#     user_id = db.Column(db.Integer)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow())
+#     question_id = db.Column(db.Integer)
+#     image_path = db.Column(db.String(200), nullable=False)
+#
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
 
 
 @app.route("/", methods=['POST', 'GET'])
@@ -26,7 +40,8 @@ def index():
     if request.method == 'POST':
         task_title = request.form['title']
         task_content = request.form['content']
-        new_task = Todo(content=task_content, title=task_title)
+        task_author = request.form['author']
+        new_task = Questions(content=task_content, title=task_title, author=task_author)
 
         try:
             db.session.add(new_task)
@@ -35,16 +50,17 @@ def index():
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
+        tasks = Questions.query.order_by(Questions.date_created).all()
         return render_template('index.html', tasks=tasks)
 
 
-@app.route("/table", methods=['POST', 'GET'])
+@app.route("/questions", methods=['POST', 'GET'])
 def table():
     if request.method == 'POST':
         task_title = request.form['title']
         task_content = request.form['content']
-        new_task = Todo(content=task_content, title=task_title)
+        task_author = request.form['author']
+        new_task = Questions(content=task_content, title=task_title, author=task_author)
 
         try:
             db.session.add(new_task)
@@ -53,13 +69,13 @@ def table():
         except:
             return 'There was an issue adding your task'
     else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('table.html', tasks=tasks)
+        tasks = Questions.query.order_by(Questions.date_created).all()
+        return render_template('questions.html', tasks=tasks)
 
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+    task_to_delete = Questions.query.get_or_404(id)
 
     try:
         db.session.delete(task_to_delete)
@@ -71,7 +87,7 @@ def delete(id):
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task = Todo.query.get_or_404(id)
+    task = Questions.query.get_or_404(id)
     if request.method == 'POST':
         task.content = request.form['content']
         task.title = request.form['title']
@@ -84,26 +100,18 @@ def update(id):
         return render_template('update.html', task=task)
 
 
-@app.route("/questions", methods=['POST', 'GET'])
-def questions_list():
-    if request.method == 'POST':
-        task_title = request.form['title']
-        task_content = request.form['content']
-        new_task = Todo(content=task_content, title=task_title)
+# @app.route("/questions")
+# def questions_list():
+#     questions = data_handler.get_all_questions()
+#     for question in questions:
+#         question["submission_time"] = datetime.datetime.fromtimestamp(int(question["submission_time"]))
+#     print(questions)
+#     return render_template("questions.html", title="Question list!", questions=questions)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/questions')
-        except:
-            return 'There was an issue adding your task'
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('questions.html', tasks=tasks)
 
 @app.route("/question/<int:id>", methods=['GET', 'POST'])
 def question_answers(id):
-    task = Todo.query.get_or_404(id)
+    task = Questions.query.get_or_404(id)
     task.view_number += 1
     try:
         db.session.commit()
@@ -114,7 +122,7 @@ def question_answers(id):
 
 @app.route("/vote_up/<int:id>", methods=['GET', 'POST'])
 def vote_up(id):
-    task = Todo.query.get_or_404(id)
+    task = Questions.query.get_or_404(id)
     task.vote_number += 1
     try:
         db.session.commit()
@@ -125,7 +133,7 @@ def vote_up(id):
 
 @app.route("/vote_down/<int:id>", methods=['GET', 'POST'])
 def vote_down(id):
-    task = Todo.query.get_or_404(id)
+    task = Questions.query.get_or_404(id)
     task.vote_number -= 1
     try:
         db.session.commit()
@@ -139,7 +147,7 @@ def add_question():
     if request.method == 'POST':
         task_title = request.form['title']
         task_content = request.form['content']
-        new_task = Todo(content=task_content, title=task_title)
+        new_task = Questions(content=task_content, title=task_title)
 
         try:
             db.session.add(new_task)
